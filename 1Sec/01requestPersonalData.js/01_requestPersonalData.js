486e8bdf4b08e9a861f00cfe7eb9430fcdde2ed5
Daniel Romero <daanmaro99june@gmail.com>
2023-03-01 07:44:44 -0600

    Use of the first code of request personal Data

---
+++ 1Sec/01requestPersonalData.js/01_requestPersonalData.js
@@ -0,0 +1,17 @@
+// Pedimos el nombre por medio de un prompt y lo guardamos en la variable "nombre"
+var nombre = prompt("Por favor, introduce tu nombre:");
+
+// Pedimos el teléfono por medio de un prompt y lo guardamos en la variable "telefono"
+var telefono = prompt("Por favor, introduce tu número de teléfono:");
+
+// Pedimos la fecha de nacimiento por medio de un prompt y la guardamos en la variable "fechaNacimiento"
+var fechaNacimiento = prompt("Por favor, introduce tu fecha de nacimiento (en formato DD/MM/AAAA):");
+
+// Creamos un mensaje que incluya los datos introducidos
+var mensaje = "Los datos introducidos son:\n\n" + 
+              "- Nombre: " + nombre + "\n" +
+              "- Teléfono: " + telefono + "\n" +
+              "- Fecha de nacimiento: " + fechaNacimiento;
+
+// Mostramos una alerta con el mensaje creado anteriormente
+alert(mensaje);
